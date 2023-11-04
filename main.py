531c2de2beb77b11f5dec7bd806de69ffa54a9a7
kvello <markus.kv1@gmail.com>
2023-11-04 13:06:15 +0800

    train.train

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import click
 import models
 import importlib
 from training import train
-import torchvision, torchvision.transforms as transforms
+import torchvision
+import torchvision.transforms as transforms
 import json
 import models
 import os
@@ -27,7 +28,7 @@ def main(train_dir, test_dir, save_model, model_path, load_model, evaluate, conf
     torch.manual_seed(seed)
 
     # Set up logging
-    if loglevel not in ['DEBUG','INFO','WARNING','ERROR','CRITICAL']:
+    if loglevel not in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
         raise ValueError('Log level {} not supported'.format(loglevel))
     # Create logger and set its level to DEBUG
     logger = logging.getLogger()
@@ -37,21 +38,22 @@ def main(train_dir, test_dir, save_model, model_path, load_model, evaluate, conf
     log_file_name = 'log'+datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'.log'
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    if not os.path.exists(os.path.join(log_dir,log_file_name)):
-        open(os.path.join(log_dir,log_file_name), 'a').close()
-    file_handler = logging.FileHandler(os.path.join(log_dir,log_file_name))
+    if not os.path.exists(os.path.join(log_dir, log_file_name)):
+        open(os.path.join(log_dir, log_file_name), 'a').close()
+    file_handler = logging.FileHandler(os.path.join(log_dir, log_file_name))
     file_handler.setLevel(logging.DEBUG)  # Capture all logs
-    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
 
     # Create and configure a console handler to only display logs from WARNING level and above
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(getattr(logging,loglevel,None))  # Only display WARNING and above
+    # Only display WARNING and above
+    console_handler.setLevel(getattr(logging, loglevel, None))
     console_formatter = logging.Formatter('%(levelname)s: %(message)s')
     console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
-
 
     if load_model:
         model = models.load_model(model_path)
@@ -64,12 +66,14 @@ def main(train_dir, test_dir, save_model, model_path, load_model, evaluate, conf
         for file in os.listdir("models"):
             if file.endswith(".py"):
                 module = importlib.import_module("models."+file[:-3])
-                if hasattr(module,model_config['type']):
-                    model = getattr(module,model_config['type'])(**model_config['args'])
+                if hasattr(module, model_config['type']):
+                    model = getattr(module, model_config['type'])(
+                        **model_config['args'])
                     found = True
                     break
-        if hasattr(torchvision.models,model_config['type']):
-            model = getattr(torchvision.models,model_config['type'])(**model_config['args'])
+        if hasattr(torchvision.models, model_config['type']):
+            model = getattr(torchvision.models, model_config['type'])(
+                **model_config['args'])
             found = True
         if not found:
             raise ValueError("Model {} not found".format(model_config['type']))
@@ -83,22 +87,27 @@ def main(train_dir, test_dir, save_model, model_path, load_model, evaluate, conf
         input_transform = []
         if "input_transform" in config:
             for transform in config["input_transform"]:
-                if hasattr(transforms,transform["type"]):
-                    input_transform.append(getattr(transforms,transform["type"])(**transform["args"]))
+                if hasattr(transforms, transform["type"]):
+                    input_transform.append(
+                        getattr(transforms, transform["type"])(**transform["args"]))
                 else:
-                    raise ValueError("Transform {} not found".format(transform))
+                    raise ValueError(
+                        "Transform {} not found".format(transform))
         input_transform = transforms.Compose(input_transform)
         output_transform = []
         if "output_transform" in config:
             for transform in config["output_transform"]:
-                if hasattr(transforms,transform["type"]):
-                    output_transform.append(getattr(transforms,transform["type"])(**transform["args"]))
+                if hasattr(transforms, transform["type"]):
+                    output_transform.append(
+                        getattr(transforms, transform["type"])(**transform["args"]))
                 else:
-                    raise ValueError("Transform {} not found".format(transform))
+                    raise ValueError(
+                        "Transform {} not found".format(transform))
         output_transform = transforms.Compose(output_transform)
-  
-        dataloader = torchvision.datasets.ImageFolder(train_dir, transform=input_transform, target_transform=output_transform)
-        train(model, dataloader, **training_config)
+
+        dataloader = torchvision.datasets.ImageFolder(
+            train_dir, transform=input_transform, target_transform=output_transform)
+        train.train(model, dataloader, **training_config)
         if save_model:
             if os.path.exists(model_path):
                 # Get current date and time
@@ -109,18 +118,22 @@ def main(train_dir, test_dir, save_model, model_path, load_model, evaluate, conf
             else:
                 torch.save(model, model_path)
     if evaluate:
-        dataloader = torchvision.datasets.ImageFolder(test_dir, transform=input_transform)
+        dataloader = torchvision.datasets.ImageFolder(
+            test_dir, transform=input_transform)
         model.eval()
         correct = 0
         total = 0
-        class_correct = list(0. for i in range(len(dataloader.dataset.classes)))
+        class_correct = list(0. for i in range(
+            len(dataloader.dataset.classes)))
         class_total = list(0. for i in range(len(dataloader.dataset.classes)))
 
         with torch.no_grad():
             for data in dataloader:
                 images, labels = data
-                images = images.to("cuda" if torch.cuda.is_available() else "cpu")
-                labels = labels.to("cuda" if torch.cuda.is_available() else "cpu")
+                images = images.to(
+                    "cuda" if torch.cuda.is_available() else "cpu")
+                labels = labels.to(
+                    "cuda" if torch.cuda.is_available() else "cpu")
                 outputs = model(images)
                 _, predicted = torch.argmax(outputs.data, 1)
                 total += labels.size(0)
@@ -131,17 +144,21 @@ def main(train_dir, test_dir, save_model, model_path, load_model, evaluate, conf
                     label = labels[i]
                     class_correct[label] += c[i].item()
                     class_total[label] += 1
-        print('Accuracy of the network on the {} test images: {} %'.format(total, 100 * correct / total))
+        print('Accuracy of the network on the {} test images: {} %'.format(
+            total, 100 * correct / total))
         with torch.no_grad():
             for data in dataloader:
                 images, labels = data
-                images = images.to("cuda" if torch.cuda.is_available() else "cpu")
-                labels = labels.to("cuda" if torch.cuda.is_available() else "cpu")
+                images = images.to(
+                    "cuda" if torch.cuda.is_available() else "cpu")
+                labels = labels.to(
+                    "cuda" if torch.cuda.is_available() else "cpu")
                 outputs = model(images)
                 _, predicted = torch.max(outputs, 1)
         for i in range(len(dataloader.dataset.classes)):
-            print('Accuracy of {} : {} %'.format(dataloader.dataset.classes[i], 100 * class_correct[i] / class_total[i]))
+            print('Accuracy of {} : {} %'.format(
+                dataloader.dataset.classes[i], 100 * class_correct[i] / class_total[i]))
+
 
 if __name__ == '__main__':
     main()
-        
